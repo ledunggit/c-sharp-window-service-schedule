81c0b9f677d54b84618d969443b6ecf62339ea0c
Lê Dũng <ledung.itsme@gmail.com>
2021-03-29 00:04:05 +0700

    comment

--- ServiceBai2/ServiceBai2.cs
+++ ServiceBai2/ServiceBai2.cs
@@ -21,10 +21,10 @@ namespace ServiceBai2
         public ServiceBai2()
         {
             InitializeComponent();
-        }      
+        }
 
         protected override void OnStart(string[] args)        {
-            
+
             timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
             timer.Interval = 10000;
             timer.Enabled = true;
@@ -35,18 +35,18 @@ namespace ServiceBai2
         {
             try
             {
-                scheduleTimer = new System.Threading.Timer(new TimerCallback(ScheduleCallBack));
-                string modeToRun = ConfigurationManager.AppSettings["Mode"].ToUpper();
-                WriteToFile("Service Mode: " + modeToRun);
-                DateTime scheduleTime = DateTime.MinValue;
-                DateTime endTime = DateTime.MinValue;
+                scheduleTimer = new System.Threading.Timer(new TimerCallback(ScheduleCallBack)); // callback sẽ nhận hàm ScheduleCallBack và tiếp tục chạy lại khi hết hàm scheduleRun
+                string modeToRun = ConfigurationManager.AppSettings["Mode"].ToUpper(); // Lấy mode trong cài đặt (file App.config)
+                WriteToFile("Service Mode: " + modeToRun); // Ghi log mode
+                DateTime scheduleTime = DateTime.MinValue; // Lấy giá trị ngày mặc định
+                DateTime endTime = DateTime.MinValue; // Giá trị ngày kết thúc là ngày mặc định
                 if (modeToRun == "DAILY")
                 {
                     scheduleTime = DateTime.Parse(ConfigurationManager.AppSettings["TimeStart"]);
                     WriteToFile(scheduleTime.ToString());
                     if (DateTime.Now > scheduleTime)
                     {
-                        scheduleTime = scheduleTime.AddDays(1);
+                        scheduleTime = scheduleTime.AddDays(1); // trường hợp lịch của user đặt nhỏ hơn ngày hiện tại (quá khứ) thì tăng lên một ngày
                     }
                     WriteToFile(scheduleTime.ToString());
                 }
@@ -56,13 +56,13 @@ namespace ServiceBai2
                     scheduleTime = DateTime.Now.AddMinutes(intervalMinutes);
                     if (DateTime.Now > scheduleTime)
                     {
-                        scheduleTime = scheduleTime.AddMinutes(intervalMinutes);
+                        scheduleTime = scheduleTime.AddMinutes(intervalMinutes); // trường hợp chạy theo lịch cách nhau một số phút, nếu số phút hiện tại nhỏ hơn số phút thời gian thực thì tăng lên
                     }
                 }
                 if (modeToRun == "STARTSTOP")
                 {
                     endTime = DateTime.Parse(ConfigurationManager.AppSettings["TimeStop"]);
-                    if (scheduleTime > DateTime.Now || DateTime.Now > endTime)
+                    if (scheduleTime > DateTime.Now || DateTime.Now > endTime) // Nếu thời gian hiện tại lớn hơn thời gian kết thúc thì dừng service
                     {
                         using (ServiceController serviceController = new ServiceController("Service"))
                         {
@@ -70,7 +70,7 @@ namespace ServiceBai2
                         }
                     }
                 }
-                if (scheduleTime < DateTime.Now)
+                if (scheduleTime < DateTime.Now) // Nếu mode không được set tức là chỉ chạy một lần, kiểm tra thời gian biểu nếu lớn hơn thời gian thực thì không thể chạy service
                 {
                     WriteToFile("The time for scheduled is earlier than now, can not set this.");
                     return;
@@ -82,13 +82,13 @@ namespace ServiceBai2
                 } else
                 {
                     timeSpan = scheduleTime.Subtract(DateTime.Now);
-                }                
+                }
                 string schedule = string.Format("{0} day {1} hour {2} minute {3} seconds", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
                 WriteToFile("Service scheduled to run after: " + schedule);
                 int dueTime = Convert.ToInt32(timeSpan.TotalMilliseconds);
-                scheduleTimer.Change(dueTime, Timeout.Infinite);                    
+                scheduleTimer.Change(dueTime, Timeout.Infinite);
             }
-            catch (Exception ex)
+            catch (Exception ex)  // gặp lỗi thì ghi log và dừng service
             {
                 WriteToFile("Error catch: " + ex.Message);
                 using (ServiceController serviceController = new ServiceController("Service"))
@@ -100,8 +100,7 @@ namespace ServiceBai2
 
         private void ScheduleCallBack(object state)
         {
-            
-            Process.Start("notepad");
+            Process.Start("notepad"); // mở process notepad nhưng không mở kèm input file
             scheduleRun();
             WriteToFile("Service restart by scheduled successfully.");
         }
@@ -109,18 +108,18 @@ namespace ServiceBai2
         private void OnElapsedTime(object sender, ElapsedEventArgs e)
         {
             WriteToFile("Rechecking ...");
-            Process[] pname = Process.GetProcessesByName("notepad");
-            if (pname.Length == 0)
+            Process[] pname = Process.GetProcessesByName("notepad"); // lấy thông tin của toàn bộ process có tên là notepad
+            if (pname.Length == 0) // check notepad có chạy không
             {
                 WriteToFile("Notepad not running!");
             }
             else
             {
                 WriteToFile("Notepad running!");
-            }            
+            }
         }
 
-        private void WriteToFile(string v)
+        private void WriteToFile(string v) // hàm ghi logs sử dụng StreamWriter
         {
             string path = AppDomain.CurrentDomain.BaseDirectory + "\\logs";
             if (!Directory.Exists(path))
@@ -146,7 +145,7 @@ namespace ServiceBai2
 
         protected override void OnStop()
         {
-            WriteToFile("The service stopped!");
+            WriteToFile("The service stopped!"); // ghi log service dừng lại.
         }
     }
 }
